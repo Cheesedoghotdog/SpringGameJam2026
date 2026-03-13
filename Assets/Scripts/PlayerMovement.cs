aa668160cbb984a260a688ac4817252f3711e039
jackh <jackhotcheese@gmail.com>
2026-03-13 12:00:27 -0500

    Readability changes

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -21,16 +21,20 @@ public class PlayerMovement : MonoBehaviour
         playerrigidbody.velocity += new Vector2(Input.GetAxisRaw("Horizontal") * runAccel, 0.0f);
         if (playerrigidbody.velocity.x >= maxSpeed || playerrigidbody.velocity.x <= -maxSpeed) {
             playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x * 0.99f, playerrigidbody.velocity.y);
+            //This slows the player down if they're going above the max speed.
         }
         if (Input.GetAxisRaw("Horizontal") == 0.0f) {
             playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x * 0.9f, playerrigidbody.velocity.y);
+            //This slows the player down even faster if they aren't pressing anything.
         }
 
         if (((Input.GetButtonDown("Jump") || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.W)) && IsOnGround())) {
             playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, jumpingPower);
+            //This causes the player to jump when space, up arrow, or w are pressed.
         }
         if ((Input.GetButtonUp("Jump") || Input.GetKeyUp(KeyCode.UpArrow) || Input.GetKeyUp(KeyCode.W)) && playerrigidbody.velocity.y > 0f) {
             playerrigidbody.velocity = new Vector2(playerrigidbody.velocity.x, playerrigidbody.velocity.y * 0.5f);
+            //This extends the player jump if they hold the button down.
 
         }
     }
@@ -38,5 +42,6 @@ public class PlayerMovement : MonoBehaviour
     public bool IsOnGround() {
         bool isTrue = groundCheck.IsTouchingLayers(LayerMask.GetMask("Ground"));
         return isTrue;
+        //This checks if the player is on the ground.
     }
 }
